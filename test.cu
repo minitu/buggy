5e98a5f4be2be22635564d0c158b5de9fdf991c1
Jaemin Choi <jchoi157@illinois.edu>
2020-01-18 13:46:57 -0800

    Convert buggy allocator to a struct

--- test.cu
+++ test.cu
@@ -1,20 +1,18 @@
 #include <buggy.h>
 #include <thread>
 
-void test() {
-  buggy::initialize();
+void test(buggy::allocator& buggy_alloc) {
+  double* a = (double*)buggy_alloc.malloc(sizeof(double) * 128);
+  double* b = (double*)buggy_alloc.malloc(sizeof(double) * 100);
 
-  double* a = (double*)buggy::malloc(sizeof(double) * 128);
-  double* b = (double*)buggy::malloc(sizeof(double) * 100);
-
-  buggy::free(a);
-
-  //buggy::finalize();
+  buggy_alloc.free(a);
 }
 
 int main() {
-  std::thread t1(test);
-  std::thread t2(test);
+  buggy::allocator buggy_alloc;
+
+  std::thread t1(test, std::ref(buggy_alloc));
+  std::thread t2(test, std::ref(buggy_alloc));
   t1.join();
   t2.join();
 
